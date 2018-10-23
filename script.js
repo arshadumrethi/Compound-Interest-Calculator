b5d1a4fcf81e0f9dc82f2ac80f2474e8d0e41069
arshadumrethi <arshad.umrethi@gmail.com>
2018-10-23 22:50:21 +0530

    JS complete, design changes

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
-calculateBtn = document.getElementById('calculate');
+//Declare variables
+const baseAmount = document.getElementById('base-amount');
+const interestRate = document.getElementById('interest-rate');
+const period = document.getElementById('period');
+const deposit = document.getElementById('deposit');
+const intervalYearly = document.getElementById('yearly');
+const intervalQuarterly = document.getElementById('quarterly');
+const intervalMonthly = document.getElementById('monthly');
+const calculateBtn = document.getElementById('calculate');
+document.getElementById('loading').style.display = 'none'
+document.getElementById('results').style.display = 'none';
 
-calculateBtn.addEventListener('click', console.log('clicked'));
+calculateBtn.addEventListener('click', function(){
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
+    
+    setTimeout(runApp, 2000);
+
+    
+
+    function runApp () {
+        document.getElementById('loading').style.display = 'none';
+        
+        let PV = baseAmount.value;
+        let i = interestRate.value/100;
+        let n = period.value;
+        let A = deposit.value;
+    
+
+        function computeYearly () {
+            let annuity = Math.round(A * (Math.pow(1 + i, n) - 1)/ i)
+            let yearlyResult = Math.round(PV * Math.pow(1 + i, n)) + annuity;
+            let interestValue = yearlyResult - baseAmount.value;
+            
+            document.getElementById('total-value').textContent = yearlyResult.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            document.getElementById('interest-earned').textContent = interestValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            document.getElementById('results').style.display = 'block';
+        }
+
+        function computeQuarterly () {
+            let quarterlyResult = Math.round(PV * Math.pow(1 + (i / 3), n * 3));
+            let interestValue2 = quarterlyResult - PV;
+
+            document.getElementById('total-value').textContent = quarterlyResult.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            document.getElementById('interest-earned').textContent = interestValue2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            document.getElementById('results').style.display = 'block';
+        }
+        
+        function computeMonthly () {
+            let monthlyResult = Math.round(PV * Math.pow(1 + (i / 12), n * 12));
+            let interestValue3 = monthlyResult - PV;
+
+            document.getElementById('total-value').textContent = monthlyResult.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            document.getElementById('interest-earned').textContent = interestValue3.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            document.getElementById('results').style.display = 'block';
+        }
+        
+        if (intervalYearly.checked == true) {
+            computeYearly()
+        } else if (intervalQuarterly.checked == true) {
+            computeQuarterly()
+        } else if (intervalMonthly.checked == true) {
+            computeMonthly()
+        } else {
+            console.log('none selected')
+        }
+
+    }
+});
+
